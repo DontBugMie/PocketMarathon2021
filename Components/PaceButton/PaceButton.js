09b1bcfc878d4133271daa12bfee5c5fb5d17102
Maya Wright <maya.wright@thoughtworks.com>
2022-01-03 13:15:11 +0000

    ready to make the pace buttons click into text input buttons module

--- Components/PaceButton/PaceButton.js
+++ Components/PaceButton/PaceButton.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import { View, StyleSheet, Text, Button} from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import {PrimaryTextColor} from "../Styles/AppStyles";
 
 const PaceButton = props => {
 
     return (
-        <View style={styles.PaceButtonContainer}>
-            <LinearGradient
-            elevation={5}
-            colors= {[props.PaceButtonBackgroundColorOne,props.PaceButtonBackgroundColorTwo]}
-            start={{ x: 0, y: 0}}
-            end={{ x: 1, y: 0.9 }}
-            style={[styles.linearGradientStyle, styles.PaceButton]}
-        >
-            <Text style={styles.PaceButtonText}>{props.PaceButtonTitle}</Text>
-            {/*might need to get rid of this line as might not be useful */}
-            <Button title={props.PaceButtonTitle}/> 
-        </LinearGradient>
+        <View style={[styles.PaceButtonContainer, styles.PaceButton]}>
+            <TouchableOpacity onPress={props.PaceButtonPressedFunc} style={[styles.PaceButton]}>
+                    <LinearGradient
+                    elevation={5}
+                    colors= {[props.PaceButtonBackgroundColorOne,props.PaceButtonBackgroundColorTwo]}
+                    start={{ x: 0, y: 0}}
+                    end={{ x: 1, y: 0.9 }}
+                    style={[styles.linearGradientStyle, styles.PaceButton]}>
+                    <Text style={styles.PaceButtonText}>{props.PaceButtonTitle}</Text>
+                </LinearGradient>
+            </TouchableOpacity>
         </View>
-        
     )
 };
 
@@ -39,8 +39,10 @@ const styles = StyleSheet.create({
     },
     PaceButtonText:{
         textAlign: 'center',
-        lineHeight: 100,
-        fontSize: 20
+        padding:50,
+        lineHeight: 10,
+        fontSize: 20,
+        color: PrimaryTextColor
     }
 });
 
